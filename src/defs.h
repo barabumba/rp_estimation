afb5468a77f7c3e8fe633306f5a31c54a2bd3d25
mturbin <turbin94@gmail.com>
2020-10-04 11:14:26 +0300

    up

--- src/defs.h
+++ src/defs.h
@@ -16,14 +16,14 @@
 
 #define N 0x4000
 #define M 0x800
-#define Q 10.
+#define Q 1.
 
 #define M_SRCH_RANGE_LEN 0x80
 #define Q_SRCH_RANGE_LEN 0x40
-#define M_SRCH_MIN M/2
-#define M_SRCH_MAX 3*M/2
-#define Q_SRCH_MIN Q/2
-#define Q_SRCH_MAX 3*Q/2
+#define M_SRCH_MIN 7*M/8
+#define M_SRCH_MAX 9*M/8
+#define Q_SRCH_MIN 7*Q/8
+#define Q_SRCH_MAX 9*Q/8
 
 #define NUMBER_OF_TESTS 1000000 
 
